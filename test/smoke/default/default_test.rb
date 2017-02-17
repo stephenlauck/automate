5ab1f0b3a07c88f7095be656ff8c5359219a2420
Stephen Lauck <lauck@chef.io>
2017-02-17 09:35:38 -0500

    Add smoke tests
    test for status endpoint (not working because of self signed cert)
    automate-ctl status

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -8,3 +8,13 @@
 describe http('https://33.33.33.10/api/_status') do
   its('status') { should cmp 200 }
 end
+
+describe command('automate-ctl status') do
+  its('stdout') { should match(/run: delivery:/) }
+  its('stdout') { should match(/run: elasticsearch:/) }
+  its('stdout') { should match(/run: kibana:/) }
+  its('stdout') { should match(/run: logstash:/) }
+  its('stdout') { should match(/run: nginx:/) }
+  its('stdout') { should match(/run: postgresql:/) }
+  its('stdout') { should match(/run: rabbitmq:/) }
+end
